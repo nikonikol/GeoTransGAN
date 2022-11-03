d394a5623751865c6cfcb726fd68f0c13d1ede36
lovelifelovefamily <2538155012@qq.com>
2022-11-03 21:24:16 +0800

    输出csv可视化

--- image_dataset.py
+++ image_dataset.py
@@ -37,27 +37,3 @@ class InfiniteSampler(Sampler):
             for idx in torch.randperm(self.N):
                 yield idx
 
-def handle_3Ddata(trainning_data_path,label_data_path,trainning_data_name,training_data_format,label_data_format,standard_x,standard_y,standard_z,RGB_data = False):#直接将输入的数据变为单个文件的路径，批量的处理写在外面
-
-    trainning_data_path = trainning_data_path + trainning_data_name + training_data_format
-
-    trainning_data_arr = read_3Dfile(trainning_data_path)
-    mask = np.zeros(trainning_data_arr.shape)
-    mask[:,12,:] = 1#数据掩膜
-    mask[:,24,:] = 1
-    mask[:,:,12] = 1
-    mask[:,:,24] = 1
-    label_data_arr = trainning_data_arr#标签
-    trainning_data_arr=mask*trainning_data_arr#条件数据
-
-    if RGB_data == False:
-        trainning_data_arr = np.expand_dims(np.array(trainning_data_arr).astype(np.float32),axis=3)
-        trainning_data_arr = np.concatenate([trainning_data_arr, trainning_data_arr, trainning_data_arr], axis=3)
-        label_data_arr = np.expand_dims(np.array(label_data_arr).astype(np.float32),axis=3)
-        label_data_arr = np.concatenate([label_data_arr, label_data_arr, label_data_arr], axis=3)
-
-    #进行归一化处理（便于收敛）先归到0-1，然后调整到-1到1
-    trainning_data_arr = trainning_data_arr  / 5. - 1
-    label_data_arr = label_data_arr / 5. - 1
-
-    return trainning_data_arr,label_data_arr
